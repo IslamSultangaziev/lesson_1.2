f4260f985df68d7c571f1efb829c9274e055dc93
IslamSultangaziev <islamsultangaziev09@gmail.com>
2026-09-16 14:11:16 +0600

    add homework

---
+++ main.js
@@ -0,0 +1,30 @@
+//1//
+var name = ("Ислам");
+var age = (18);
+var isStudent = true;
+var emptyValue = null;
+var notDefined;
+//2///
+console.log(name);
+console.log(age);
+console.log(isStudent);
+console.log(emptyValue);
+console.log(notDefined);
+//3//
+console.log(typeof name);
+console.log(typeof age);
+console.log(typeof isStudent);
+console.log(typeof emptyValue);
+console.log(typeof notDefined);
+//4//
+const person = {
+    name: "Ислам",
+    age: 18,
+    isStudent: true
+};
+//5//
+console.log("Меня зовут " + person.name + ", мне " + person.age + " лет.");
+
+//6//
+let favoriteMovies =["Трансформеры","Проект-А","Темный-рыцарь","Час-Пик","Оно"]
+console.log(favoriteMovies)
